7af8d90180433ac4d179086fc83b56f527e0e16b
Umar Farooq <ufrajput@gmail.com>
2020-06-17 10:58:23 +0500

    .

--- init.R
+++ init.R
@@ -1,9 +1,4 @@
-> # We can use the print() function
-> print("Hello World!")
-[1] "Hello World!"
-> # Quotes can be suppressed in the output
-> print("Hello World!", quote = FALSE)
-[1] Hello World!
-> # If there are more than 1 item, we can concatenate using paste()
-> print(paste("How","are","you?"))
-[1] "How are you?"
+# My first program in R Programming
+myString <- "Hello, World!"
+
+print ( myString)
